855ebffee44b39878769388f78128f0292bbfdc1
sandeep <sandeep@accentuateglobal.in>
2018-10-15 12:48:02 +0530

    tt

--- crud/application/models/main_model.php
+++ crud/application/models/main_model.php
@@ -7,35 +7,35 @@
       }  
       function insert_data($data)  
       {  
-           $this->db->insert("tbl_user", $data);  
+           $this->db->insert("tbl_users", $data);  
       }  
       function fetch_data()  
       {  
-           //$query = $this->db->get("tbl_user");  
-           //select * from tbl_user  
-           //$query = $this->db->query("SELECT * FROM tbl_user ORDER BY id DESC");  
+           //$query = $this->db->get("tbl_users");  
+           //select * from tbl_users  
+           //$query = $this->db->query("SELECT * FROM tbl_users ORDER BY id DESC");  
 		
            $this->db->select("*");  
-           $this->db->from("tbl_user");  
+           $this->db->from("tbl_users");  
            $query = $this->db->get();  
            return $query;  
       }  
       function delete_data($id){  
            $this->db->where("id", $id);  
-           $this->db->delete("tbl_user");  
-           //DELETE FROM tbl_user WHERE id = $id  
+           $this->db->delete("tbl_users");  
+           //DELETE FROM tbl_users WHERE id = $id  
       }  
       function fetch_single_data($id)  
       {  
            $this->db->where("id", $id);  
-           $query = $this->db->get("tbl_user");  
+           $query = $this->db->get("tbl_users");  
            return $query;  
-           //Select * FROM tbl_user where id = '$id'  
+           //Select * FROM tbl_users where id = '$id'  
       }  
       function update_data($data, $id)  
       {  
            $this->db->where("id", $id);  
-           $this->db->update("tbl_user", $data);  
-           //UPDATE tbl_user SET first_name = '$first_name', last_name = '$last_name' WHERE id = '$id'  
+           $this->db->update("tbl_users", $data);  
+           //UPDATE tbl_users SET first_name = '$first_name', last_name = '$last_name' WHERE id = '$id'  
       } 
  }
