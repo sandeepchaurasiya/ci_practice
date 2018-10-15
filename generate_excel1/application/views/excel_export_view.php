c52808596c609489deb5edcff6880e70154f6276
sandeep <sandeep@accentuateglobal.in>
2018-10-15 19:20:11 +0530

    Export excel

---
+++ generate_excel1/application/views/excel_export_view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Export Data to Excel in Codeigniter using PHPExcel</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+<div class="container box">
+	<h3 align="center">Export Data to Excel in Codeigniter using PHPExcel</h3>
+	<br />
+	<div class="table-responsive">
+	<table class="table table-bordered">
+		<tr>
+			<th>Name</th>
+			<th>Address</th>
+			<th>Gender</th>
+			<th>Designation</th>
+			<th>Age</th>
+		</tr>
+		<?php
+		foreach($employee_data as $row){
+			echo'
+			<tr>
+				<td>'.$row->name.'</td>
+				<td>'.$row->address.'</td>
+				<td>'.$row->gender.'</td>
+				<td>'.$row->designation.'</td>
+				<td>'.$row->age.'</td>
+			</tr>';
+		}
+		?>
+	</table>
+	<div align="center">
+		<form method="post" action="<?php echo base_url();?>excel_export/action">
+			<input type="submit" name="export" value="Export" class="btn btn-success" />
+		</form>
+	</div>
+	</div>
+</div>
+	
+</body>
+</html>
