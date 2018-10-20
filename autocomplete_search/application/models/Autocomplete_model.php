541439088e971d550787f48dbc6249f5a0e36531
sandeep <sandeepchaurasiya9211@gmail.com>
2018-10-21 03:40:22 +0530

    auto

---
+++ autocomplete_search/application/models/Autocomplete_model.php
@@ -0,0 +1,22 @@
+<?php
+class Autocomplete_model extends CI_Model
+{
+ function fetch_data($query)
+ {
+  $this->db->like('student_name', $query);
+  $query = $this->db->get('tbl_student');
+  if($query->num_rows() > 0)
+  {
+   foreach($query->result_array() as $row)
+   {
+    $output[] = array(
+     'name'  => $row["student_name"],
+     'image'  => $row["image"]
+    );
+   }
+   echo json_encode($output);
+  }
+ }
+}
+
+?>
