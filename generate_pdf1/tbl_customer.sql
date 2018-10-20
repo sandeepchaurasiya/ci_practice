077d7966a091ba05bd6c451fe418b5d4aa11709f
sandeep <sandeepchaurasiya9211@gmail.com>
2018-10-21 02:27:27 +0530

    update

---
+++ generate_pdf1/tbl_customer.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2018 at 03:58 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `CustomerID` int(11) NOT NULL,
+  `CustomerName` varchar(250) NOT NULL,
+  `Address` text NOT NULL,
+  `City` varchar(250) NOT NULL,
+  `PostalCode` varchar(30) NOT NULL,
+  `Country` varchar(100) NOT NULL,
+  `images` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_customer`
+--
+
+INSERT INTO `tbl_customer` (`CustomerID`, `CustomerName`, `Address`, `City`, `PostalCode`, `Country`, `images`) VALUES
+(1, 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany', '1.png'),
+(2, 'Ana Trujillo', 'Avda. de la Construction 2222', 'Mexico D.F.', '5021', 'Mexico', '2.jpg'),
+(3, 'Antonio Moreno', 'Mataderos 2312', 'Mexico D.F.', '5023', 'Mexico', '3.jpg'),
+(4, 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK', '4.jpg'),
+(5, 'Paula Parente', 'Rua do Mercado, 12', 'Resende', '08737-363', 'Brazil', '5.jpg'),
+(6, 'Wolski Zbyszek', 'ul. Filtrowa 68', 'Walla', '01-012', 'Poland', '2.jpg'),
+(7, 'Matti Karttunen', 'Keskuskatu 45', 'Helsinki', '21240', 'Finland', '3.jpg'),
+(8, 'Karl Jablonski', '305 - 14th Ave. S. Suite 3B', 'Seattle', '98128', 'USA', '4.jpg'),
+(9, 'Paula Parente', 'Rua do Mercado, 12', 'Resende', '08737-363', 'Brazil', '5.jpg'),
+(10, 'Pirkko Koskitalo', 'Torikatu 38', 'Oulu', '90110', 'Finland', '1.png'),
+(11, 'Ronald Bowne', '2343 Shadowmar Drive', 'New Orleans', '70112', 'United States', '2.jpg'),
+(12, 'Joyce Rosenberry', 'Norra Esplanaden 56', 'HELSINKI', '00380', 'Finland', '3.jpg'),
+(13, 'Jeff Putnam', 'Industrieweg 56', 'Bouvignies', '7803', 'Belgium', '5.jpg'),
+(14, 'Trina Davidson', '1049 Lockhart Drive', 'Barrie', 'ON L4M 3B1', 'Canada', '4.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
