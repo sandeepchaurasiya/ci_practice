5cd5599529d5a4fb93568c5b4959a1447f64758c
sandeep <sandeepchaurasiya9211@gmail.com>
2018-10-21 17:16:05 +0530

    2 proj uptated

---
+++ load_more_data_on_page_scroll1/application/models/Scroll_pagination_model.php
@@ -0,0 +1,16 @@
+<?php
+class Scroll_pagination_model extract CI_Model
+{
+	function fetch_data($limit,$start)
+	{
+		$this->db->select("*");
+		$this->db->from("post");
+		$this->db->order_by("id","DESC");
+		$this->db->limit($limit,$start);
+		$query = $this->db->get();
+		return $query;
+		
+	}
+}
+
+?>
