e1c2232550d21e73d8739b9f8ac29e1462655a24
sandeep <sandeepchaurasiya9211@gmail.com>
2018-10-22 01:49:45 +0530

    data_enc_dec not working

---
+++ data_encry_decry/application/models/EncryptionDecryption_model.php
@@ -0,0 +1,30 @@
+<?php
+class EncryptionDecryption_model extends CI_Model
+{
+ function insert($data)
+ {
+  $this->db->insert('sample_data', $data);
+ }
+
+ function fetch_data()
+ {
+  $this->db->order_by('id', 'DESC');
+  $query = $this->db->get('sample_data');
+  return $query;
+ }
+
+ function fetch_single_data($id)
+ {
+  $this->db->where('id', $id);
+  return $this->db->get('sample_data');
+ }
+
+ function edit($id, $data)
+ {
+  $this->db->where('id', $id);
+  $this->db->update('sample_data', $data);
+ }
+}
+
+?>
+
---
+++ data_encry_decry/application/views/insert_data.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <title>Codeigniter Encryption and Decryption - Insert Data</title>
+    
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <style>
+    body
+    {
+     background-color: #f1f1f1;
+    }
+    .box
+    {
+     width: 600px;
+     margin:0 auto;
+     background-color: #fff;
+     border:1px solid #ccc;
+     border-radius: 5px;
+     padding:16px;
+    }
+ </style>
+</head>
+<body>
+ <div class="container">
+  <br />
+  <br />
+  <div class="box">
+   <h3 align="center">Codeigniter3 Encryption and Decryption - Insert Data</h3>
+   <br />
+            <?php
+            if(validation_errors() != '')
+            {
+                echo '
+                <div class="alert alert-danger alert-dismissible">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    ' . validation_errors() .'
+                </div>
+                ';
+            }
+            ?>
+   <form method="post" action="<?php echo base_url(); ?>encryptiondecryption/insert_validation">
+                <input type="text" name="first_name" class="form-control" placeholder="Enter First Name" />
+                <br />
+                <input type="text" name="last_name" class="form-control" placeholder="Enter Last Name" />
+                <br />
+                <input type="text" name="age" class="form-control" placeholder="Enter Age" />
+                <br />
+                <select name="gender" class="form-control">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                <br />
+                <div align="center">
+                    <input type="submit" name="insert" class="btn btn-primary" value="Insert" />
+                </div>
+            </form>
+  </div>
+  <br />
+ </div>
+</body>
+</html>
