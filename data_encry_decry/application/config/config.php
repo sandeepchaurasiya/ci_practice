713673820e6312e0b4be7a5ca88def6fa94e71d2
sandeep <sandeep@accentuateglobal.in>
2018-10-22 16:04:53 +0530

    data enc

--- data_encry_decry/application/config/config.php
+++ data_encry_decry/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/ci_practice/data_encry_decry/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+//$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'zq8Li15edjb0S4dimvQz3JQXcKGGmKdV';
+$config['encryption_key'] = 'xRUqKhsoZ5qV6y3kqARFJFdPqJvp7X2z';
 
 /*
 |--------------------------------------------------------------------------
