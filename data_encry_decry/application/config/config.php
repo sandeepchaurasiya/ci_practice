9db68dafc8f166422e06feb2cbb459c938e75f35
sandeep <sandeepchaurasiya9211@gmail.com>
2019-01-05 01:06:57 +0530

    hh

--- data_encry_decry/application/config/config.php
+++ data_encry_decry/application/config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'xRUqKhsoZ5qV6y3kqARFJFdPqJvp7X2z';
+$config['encryption_key'] = 'gf3t2RVmoy';
 
 /*
 |--------------------------------------------------------------------------
