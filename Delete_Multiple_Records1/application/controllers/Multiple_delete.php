c2b55ec6276d3d844059041bc95fdf9a4fe6badc
sandeep <sandeepchaurasiya9211@gmail.com>
2019-01-08 22:24:38 +0530

    update

---
+++ Delete_Multiple_Records1/application/controllers/Multiple_delete.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Multiple_delete extends CI_Controller{
+	
+	public function __construct() {
+            parent::__construct();
+            $this->load->model('multiple_delete_model');
+        }
+        function index(){
+            $data['employee_data'] = $this->multiple_delete_model->fetch_all();
+            $this->load->view("multiple_delete",$data);
+        }
+        function delete_all(){
+            if($this->input->post('checkbox_value')){
+                $id = $this->input->post('checkbox_value');
+                for($count = 0; $count < count($id); $count++){
+                    $this->multiple_delete_model->delete($id[$count]);
+                }
+                
+            }
+        }
+}
+?>
---
+++ Delete_Multiple_Records1/application/models/Multiple_delete_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Multiple_delete_model extends CI_Model{
+    function fetch_all(){
+        $this->db->select("*");
+        $this->db->from("tbl_employee");
+        return $this->db->get();
+    }
+    function delete($id){
+        $this->db->where('id',$id);
+        $this->db->delete('tbl_employee');
+    }
+    
+}
